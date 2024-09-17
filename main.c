09556b76ba1bc550674cb405d9baf46a553361d9
yurimds10 <coder73854@proton.me>
2024-09-17 00:38:38 -0300

    :sparkles: feat: add array in loops

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
-int main() {
-  return 0;
+void printArrayWithoutLoop(double prices[]) {
+
+  // without loop
+  printf("$%.2lf\n", prices[0]);
+  printf("$%.2lf\n", prices[1]);
+  printf("$%.2lf\n", prices[2]);
+  printf("$%.2lf\n", prices[3]);
+  printf("$%.2lf\n", prices[4]);
 }
 
+void printArrayWithLoop(double prices[]) {
+
+  // with for loop
+  for (int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++) {
+    printf("$%.2lf\n", prices[i]);
+  }
+}
+
+int main() {
+  double prices[] = {5.0, 10.0, 15.0, 20.0, 25.0, 30.0};
+
+  //printArrayWithoutLoop(prices);
+  printArrayWithLoop(prices);
+
+  return 0;
+}
