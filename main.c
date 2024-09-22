a46eb5ee1dc330b1db0f2114e9c3e4d5f7d67669
yurimds10 <coder73854@proton.me>
2024-09-22 01:25:23 -0300

    :book: studies: add bubble sort lesson

--- main.c
+++ main.c
@@ -1,28 +1,7 @@
 #include <stdio.h>
 
-void printArrayWithoutLoop(double prices[]) {
-
-  // without loop
-  printf("$%.2lf\n", prices[0]);
-  printf("$%.2lf\n", prices[1]);
-  printf("$%.2lf\n", prices[2]);
-  printf("$%.2lf\n", prices[3]);
-  printf("$%.2lf\n", prices[4]);
-}
-
-void printArrayWithLoop(double prices[]) {
-
-  // with for loop
-  for (int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++) {
-    printf("$%.2lf\n", prices[i]);
-  }
-}
-
-int main() {
-  double prices[] = {5.0, 10.0, 15.0, 20.0, 25.0, 30.0};
-
-  //printArrayWithoutLoop(prices);
-  printArrayWithLoop(prices);
-
+int main()
+{
+  printf("Hello");
   return 0;
 }
